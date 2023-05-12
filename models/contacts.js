35adc28156b9ada10f8011b761468739f1eada3c
MaxKorovchenko <max.korovchenko@gmail.com>
2023-05-12 20:30:29 +0300

    add functions to work with contacts.json

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,64 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const getAllContactsService = async () => {
+  const contacts = await fs.readFile(contactsPath);
 
-const removeContact = async (contactId) => {}
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+const getContactService = async (id) => {
+  const contacts = await getAllContactsService();
+  const contact = contacts.find((contact) => contact.id === id);
 
-const updateContact = async (contactId, body) => {}
+  return contact || null;
+};
+
+const addContactService = async (body) => {
+  const contacts = await getAllContactsService();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+};
+
+const updateContactService = async (id, body) => {
+  const contacts = await getAllContactsService();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index === -1) return null;
+
+  contacts[index] = {
+    id,
+    ...body,
+  };
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return contacts[index];
+};
+
+const removeContactService = async (id) => {
+  const contacts = await getAllContactsService();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index === -1) return null;
+
+  contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return id;
+};
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+  getAllContactsService,
+  getContactService,
+  addContactService,
+  updateContactService,
+  removeContactService,
+};
