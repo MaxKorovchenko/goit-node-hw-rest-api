f5eca8f51765f5815c7118480f5e491f8e13af7d
MaxKorovchenko <max.korovchenko@gmail.com>
2023-06-08 14:23:11 +0300

    add sendEmail by using nodemailer

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,26 +1,44 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const gravatar = require("gravatar");
-const path = require("path");
-const fs = require("fs/promises");
-const Jimp = require("jimp");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const path = require('path');
+const fs = require('fs/promises');
+const Jimp = require('jimp');
+const { nanoid } = require('nanoid');
 
-const { HttpError } = require("../helpers/HttpError");
-const { controllerWrapper } = require("../helpers/controllerWrapper");
-const { User } = require("../models/user");
+const { HttpError } = require('../helpers/HttpError');
+const { controllerWrapper } = require('../helpers/controllerWrapper');
+const { User } = require('../models/user');
+const { sendEmail } = require('../helpers/sendEmail');
+
+const { SECRET_KEY, PROJECT_URL } = process.env;
 
 const register = controllerWrapper(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
-    throw new HttpError(409, "Email already in use");
+    throw new HttpError(409, 'Email already in use');
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationCode = nanoid();
 
-  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+    verificationCode,
+  });
+
+  const verifyEmail = {
+    to: email,
+    subject: 'Email verification',
+    html: `<a href="${PROJECT_URL}/api/auth/verify/${verificationCode}" target="_blank">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     name: newUser.name,
@@ -33,20 +51,20 @@ const login = controllerWrapper(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw new HttpError(401, "Email or password invalid");
+    throw new HttpError(401, 'Email or password invalid');
   }
 
   const passwordCompare = await bcrypt.compare(password, user.password);
 
   if (!passwordCompare) {
-    throw new HttpError(401, "Email or password invalid");
+    throw new HttpError(401, 'Email or password invalid');
   }
 
   const payload = {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "22h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '22h' });
   await User.findByIdAndUpdate(user._id, { token });
 
   res.json({ token });
@@ -54,10 +72,10 @@ const login = controllerWrapper(async (req, res) => {
 
 const logout = controllerWrapper(async (req, res) => {
   const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: "" });
+  await User.findByIdAndUpdate(_id, { token: '' });
 
   res.json({
-    message: "Logout success",
+    message: 'Logout success',
   });
 });
 
@@ -78,7 +96,7 @@ const updateSubscription = controllerWrapper(async (req, res) => {
   res.status(200).json(user);
 });
 
-const avatarsDir = path.join(__dirname, "..", "public", "avatars");
+const avatarsDir = path.join(__dirname, '..', 'public', 'avatars');
 
 const updateAvatar = controllerWrapper(async (req, res) => {
   const { _id } = req.user;
@@ -92,7 +110,7 @@ const updateAvatar = controllerWrapper(async (req, res) => {
   const newPath = path.join(avatarsDir, filename);
   await fs.rename(tempPath, newPath);
 
-  const avatarURL = path.join("avatars", filename);
+  const avatarURL = path.join('avatars', filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.status(200).json({
